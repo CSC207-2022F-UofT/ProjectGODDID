e18ef57b92243096b83c59fa4a89d7402cad8d34
BYT18 <hockeybyt18@gmail.com>
2022-11-15 13:55:15 -0500

    friend recomendation system

--- src/main/java/useCases/FriendRecommender.java
+++ src/main/java/useCases/FriendRecommender.java
@@ -1,8 +1,11 @@
 package useCases;
 
-import entities.User;
+import entities.*;
+
+//import useCases.AccountManager.*;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class FriendRecommender {
     public FriendRecommender(){
@@ -15,6 +18,36 @@ public class FriendRecommender {
             int randomNum = (int)(Math.random()*user.getFriends().size());
             recs.add(user.getFriends().get(randomNum));
         }
+        return recs;
+    }
+
+    public Vertex getRecommend(Vertex user, Graph allUsers){
+        Vertex recs = new Vertex("","",new User());
+        Map<Vertex, Integer> friends = new HashMap<>();
+
+        for (Vertex i : allUsers.accounts.get(user)) {
+            //friends.putIfAbsent(i, 0);
+            for (Vertex j : allUsers.accounts.get(i)) {
+                if (friends.get(j) == null) {
+                    friends.putIfAbsent(i, 0);
+                } else {
+                    friends.put(j, friends.get(j) + 1);
+                }
+                //recs.add(user.getFriends().get(randomNum));
+            }
+        }
+
+        int currMax = 0;
+        for (Vertex a : friends.keySet()){
+            if (friends.get(a) > currMax){
+                for(Vertex keys : friends.keySet()) {
+                    if(friends.get(keys).equals(friends.get(a))) {
+                        recs = keys;
+                    }
+                }
+            }
+        }
+
         return recs;
     }
 
