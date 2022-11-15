7683e07d8a9fbef85b883ed7d0c5ac2a7a5f0e88
BYT18 <hockeybyt18@gmail.com>
2022-11-15 15:56:24 -0500

    friend adder and docstring

--- src/main/java/useCases/FriendAdder.java
+++ src/main/java/useCases/FriendAdder.java
@@ -1,14 +1,20 @@
 package useCases;
 
-import entities.User;
+import entities.*;
 
+/*
+Adds friend to current users list of friends as well as manipulates graph to create a new edge between the current user and the added friend
+*/
 public class FriendAdder {
     public FriendAdder(){
-
     }
 
-    public void add(User currUser, User friend)
+    /*
+
+    */
+    public void add(User currUser, User friend, Graph allUsers)
     {
         currUser.getFriends().add(friend);
+        allUsers.addEdge(currUser.getUsername(),currUser.getAccountType(), currUser, friend.getUsername(), friend.getAccountType(), friend);
     }
 }
--- src/main/java/useCases/FriendRecommender.java
+++ src/main/java/useCases/FriendRecommender.java
@@ -1,17 +1,20 @@
 package useCases;
 
 import entities.*;
-
-//import useCases.AccountManager.*;
-
 import java.util.ArrayList;
 import java.util.*;
 
+/*
+Automated friend recommender system where a friend is recommended based on mutual friend count.
+This use case occurs when a user interacts with the friend adder UI / interfaces.
+*/
 public class FriendRecommender {
     public FriendRecommender(){
-
     }
 
+    /*
+    Given a user, randomly select three of their friends to recommend back to the current user.
+    */
     public ArrayList<User> getRecommendation(User user){
         ArrayList<User> recs = new ArrayList<>();
         for (int i = 0; i < 3; i ++) {
@@ -21,6 +24,10 @@ public class FriendRecommender {
         return recs;
     }
 
+    /*
+    Given a Vertex which represents a user and a Graph representing all the users in the app, search graph of
+    users and finds highest count of mutual friends among the users neighbours.
+    */
     public Vertex getRecommend(Vertex user, Graph allUsers){
         Vertex recs = new Vertex("","",new User());
         Map<Vertex, Integer> friends = new HashMap<>();
