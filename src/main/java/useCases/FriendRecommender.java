2960dd8b6c0a939e4c66d7a5aaa6d9531e319557
Asr161 <ashvat.ranadive@mail.utoronto.ca>
2022-11-17 09:02:50 -0500

    added FriendAdderTest and FriendRemoverTest

--- src/main/java/useCases/FriendRecommender.java
+++ src/main/java/useCases/FriendRecommender.java
@@ -10,13 +10,14 @@ This use case occurs when a user interacts with the friend adder UI / interfaces
 */
 public class FriendRecommender {
     private User recs; //Made change here
-    public FriendRecommender(){
+
+    public FriendRecommender() {
     }
 
     /*
     Given a user, randomly select three of their friends to recommend back to the current user.
     */
-    public ArrayList<User> getRecommendation(User user){
+    public ArrayList<User> getRecommendation(User user) {
         if (user.getFriends().size() >= 1) {
             ArrayList<User> recs = new ArrayList<>();
             for (int i = 0; i < 3; i++) {
@@ -33,43 +34,48 @@ public class FriendRecommender {
     Given a Vertex which represents a user and a Graph representing all the users in the app, search graph of
     users and finds highest count of mutual friends among the users neighbours.
     */
-    public User getRecommend(User user, Graph allUsers){
+    public User getRecommend(User user, Graph allUsers) {
         //Vertex recs = new Vertex("","",new User());
-        Map<User, Integer> friends = new HashMap<>();
-        for (User i : allUsers.accounts.get(user)) {
-            //friends.putIfAbsent(i, 0);
-            System.out.println(allUsers.accounts.get(i));
-            int counter = 0;
-            for (User j : allUsers.accounts.get(i)) {
-                System.out.println(friends.containsKey(j));
-                if (friends.containsKey(j) == false) {
-                    friends.putIfAbsent(allUsers.accounts.get(i).get(counter), 0);
-                } else {
-                    friends.put(j, friends.get(j) + 1);
+        if (user != null && allUsers != null) {
+            Map<User, Integer> friends = new HashMap<>();
+            for (User i : allUsers.accounts.get(user)) {
+                //friends.putIfAbsent(i, 0);
+                System.out.println(allUsers.accounts.get(i));
+                int counter = 0;
+                for (User j : allUsers.accounts.get(i)) {
+                    System.out.println(friends.containsKey(j));
+                    if (friends.containsKey(j) == false) {
+                        friends.putIfAbsent(allUsers.accounts.get(i).get(counter), 0);
+                    } else {
+                        friends.put(j, friends.get(j) + 1);
+                    }
+                    counter++;
+                    //recs.add(user.getFriends().get(randomNum));
                 }
-                counter ++;
-                //recs.add(user.getFriends().get(randomNum));
             }
-        }
 
-        //System.out.println("working");
-        System.out.println(friends.keySet());
-        int currMax = 0;
-        for (User a : friends.keySet()){
-            if (friends.get(a) >= currMax){
-                System.out.println(friends.get(a));
-                currMax = friends.get(a);
-                System.out.println(currMax);
-                for(User keys : friends.keySet()) {
-                    if(friends.get(keys).equals(friends.get(a))) {
-                        recs = keys;
-                        //System.out.println(recs);
+            //System.out.println("working");
+            System.out.println(friends.keySet());
+            int currMax = 0;
+            for (User a : friends.keySet()) {
+                if (friends.get(a) >= currMax) {
+                    System.out.println(friends.get(a));
+                    currMax = friends.get(a);
+                    System.out.println(currMax);
+                    for (User keys : friends.keySet()) {
+                        if (friends.get(keys).equals(friends.get(a))) {
+                            recs = keys;
+                            //System.out.println(recs);
+                        }
                     }
                 }
             }
+            return recs;
         }
-        return recs;
+        else return null;
+
     }
+}
 
 //    public User getRecommend(User user, Graph allUsers){
 //        //Vertex recs = new Vertex("","",new User());
@@ -111,4 +117,4 @@ public class FriendRecommender {
 //        return recs;
 //    }
 
-}
+
---
+++ src/test/java/useCases/FriendAdderTest.java
@@ -0,0 +1,80 @@
+package useCases;
+
+import entities.Graph;
+import entities.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FriendAdderTest {
+
+        AccountManager accountManager = new AccountManager();
+        User user1, user2, user3, user4, user5;
+
+        ArrayList<User> alphaFriends, deltaFriends;
+
+
+    @BeforeEach
+        void setUp() {
+
+            user1 = new User("", "");
+            user1.setUsername("Alpha");
+            user1.setAccountType("casual");
+            accountManager.addUser(user1);
+            //accountManager.addUser("Alpha", "123","mail","Casual");
+
+            user2 = new User("", "");
+            user2.setUsername("Beta");
+            user2.setAccountType("casual");
+            accountManager.addUser(user2);
+            //accountManager.addUser("Beta", "123","mail","Casual");
+
+            user3 = new User("", "");
+            user3.setUsername("Charlie");
+            user3.setAccountType("casual");
+            accountManager.addUser(user3);
+            //accountManager.addUser("Charlie", "123","mail","Casual");
+
+            user4 = new User("", "");
+            user4.setUsername("Delta");
+            user4.setAccountType("casual");
+            accountManager.addUser(user4);
+            //accountManager.addUser("Delta", "123","mail","Casual");
+
+            user5 = new User("", "");
+            user5.setUsername("Echo");
+            user5.setAccountType("casual");
+            accountManager.addUser(user5);
+            //accountManager.addUser("Echo", "123","mail","Casual");
+
+
+        }
+
+        @AfterEach
+        void tearDown() {
+        }
+
+        @Test
+        void addFriend() {
+            FriendAdder fd = new FriendAdder();
+            fd.addFriend(user1, user2, accountManager);
+            fd.addFriend(user2, user1, accountManager);
+            fd.addFriend(user5, user1, accountManager);
+            fd.addFriend(user3, user4, accountManager);
+            fd.addFriend(user3, user5, accountManager);
+            assert(user1.getFriends().contains(user2));
+            assert(user2.getFriends().contains(user1));
+            assert(user5.getFriends().contains(user1));
+            assert(user3.getFriends().contains(user4));
+            assert(user3.getFriends().contains(user5));
+        }
+
+    }
+
+
