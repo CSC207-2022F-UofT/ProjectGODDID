823e3ec2a5f0246ef1bde266b0ff1447d3d13e90
Manit2210 <gosaliamanit@gmail.com>
2022-11-17 20:24:40 -0500

    Chat Files Text Addition class added

--- src/main/java/useCases/ChatTextFiles.java
+++ src/main/java/useCases/ChatTextFiles.java
@@ -15,19 +15,32 @@ public class ChatTextFiles {
     public ChatTextFiles(){
 
     }
-    public void AddToTextFile(String s, String username){
-        try {
-            FileWriter writer = new FileWriter("file1.txt");
-            writer.write(username + ": " + s);
-            writer.close();
+    public void AddToTextFile(String messages, String username1, String username2, String usernameactiveuser){
+        String s = "src/" + username1 + username2 + ".txt";
+        String s1 = "src/" + username2 + username1 + ".txt";
+        File f = new File(s);
+        if (f.exists()) {
+            try {
+                FileWriter writer = new FileWriter(s);
+                writer.write(usernameactiveuser + ": " + messages);
+                writer.close();
+            } catch (IOException error) {
+                error.printStackTrace();
+            }
         }
-        catch (IOException error){
-            error.printStackTrace();
+        else{
+            try {
+                FileWriter writer = new FileWriter(s1);
+                writer.write(usernameactiveuser + ": " + messages);
+                writer.close();
+            } catch (IOException error) {
+                error.printStackTrace();
+            }
         }
     }
 
     public void CreateTextFile(String username1, String username2) throws IOException {
-        String s = "src/" + username1 + username2;
+        String s = "src/" + username1 + username2 + ".txt";
         File file = new File(s);
             if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
@@ -41,25 +54,43 @@ public class ChatTextFiles {
 
     public ArrayList<String> ReadFiles(String username1, String username2) {
         ArrayList<String> list_of_messages = new ArrayList<String>();
-        String s = "src/" + username1 + username2;
-        String s1 = "src/" + username2 + username1;
-        try{
-            File file = Paths.get(s);
-            Scanner sc = new Scanner(file);
-            while (sc.hasNextLine()){
-                String data = sc.nextLine();
-                list_of_messages.add(data);
+        String s = "src/" + username1 + username2 + ".txt";
+        String s1 = "src/" + username2 + username1 + ".txt";
+        File f = new File(s);
+        if (f.exists()) {
+            try {
+                Scanner sc = new Scanner(f);
+                while (sc.hasNextLine()) {
+                    String data = sc.nextLine();
+                    list_of_messages.add(data);
+                }
+                sc.close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
             }
-            sc.close();
         }
-
+        else{
+            try {
+                File file = new File(s1);
+                Scanner sc = new Scanner(file);
+                while (sc.hasNextLine()) {
+                    String data = sc.nextLine();
+                    list_of_messages.add(data);
+                }
+                sc.close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return list_of_messages;
     }
 
 
+
+
     public void DeleteTextFile(String username1, String username2) throws IOException {
-        String s = "src/" + username1 + username2;
-        String s1 = "src/" + username2 + username1;
+        String s = "src/" + username1 + username2 + ".txt";
+        String s1 = "src/" + username2 + username1 + ".txt";
         Files.deleteIfExists(Paths.get(s));
         Files.deleteIfExists(Paths.get(s1));
     }
