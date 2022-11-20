0fe76e5400a097d0246ccec344a2f2cd0139a778
Jangalang2911 <malhar.v.padir@gmail.com>
2022-11-19 20:20:13 -0500

    PointSystemTest uploaded
    Exception handled as per IntelliJ's default suggestion (Ashvat's idea)

--- src/test/java/useCases/PointSystemTest.java
+++ src/test/java/useCases/PointSystemTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class FriendRecommenderTest {
+class PointSystemTest {
 
     Graph graph;
 
@@ -24,7 +25,7 @@ class FriendRecommenderTest {
 
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException, ClassNotFoundException {
         graph = new Graph();
 
         curUser = new User("", "");
@@ -57,65 +58,20 @@ class FriendRecommenderTest {
         accountManager.addUser(user5);
         //accountManager.addUser("Echo", "123","mail","Casual");
 
-        alphaFriends = new ArrayList<>();
-        alphaFriends.add(user2);
-        alphaFriends.add(user3);
-        curUser.setFriends(alphaFriends);
-        //accountManager.addFriend(curUser, user2);
-        //accountManager.addFriend(curUser,user3);
-        ArrayList<User> keys = new ArrayList<>(accountManager.getGraph().accounts.keySet());
-        accountManager.addFriend(keys.get(0), keys.get(1));
-        accountManager.addFriend(keys.get(0), keys.get(2));
-        accountManager.addFriend(keys.get(0), keys.get(3));
+
+        accountManager.addFriend(user2, user3);
+        accountManager.addFriend(user2, user4);
+        accountManager.addFriend(user2, user5);
         //System.out.println(graph.getVertices());
         //graph.addEdge("Alpha", "Casual", curUser, "Beta","Casual",user2);
         //graph.addEdge("Alpha", "Casual", curUser, "Charlie","Casual",user3);
 
-        deltaFriends = new ArrayList<>();
-        deltaFriends.add(user3);
-        deltaFriends.add(user5);
-        user5.setFriends(deltaFriends);
-        //accountManager.addFriend(user4, user3);
-        //accountManager.addFriend(user4,user5);
-        accountManager.addFriend(keys.get(3), keys.get(4));
-        accountManager.addFriend(keys.get(3), keys.get(2));
-        accountManager.addFriend(keys.get(2), keys.get(4));
-        //graph.addEdge("Delta", "Casual", user4, "Echo","Casual",user5);
-        //graph.addEdge("Delta", "Casual", user4, "Charlie","Casual",user3);
     }
 
     @AfterEach
     void tearDown() {
     }
 
-    @Test
-    void getRecommendation() {
-        ArrayList<User> keys = new ArrayList<>(accountManager.getGraph().accounts.keySet());
-        //System.out.println(graph.accounts.get(keys.get(0)));
-        //System.out.println(user5.getFriends());
-        FriendRecommender fRec = new FriendRecommender();
-        ArrayList<User> recs = new ArrayList<>();
-        recs = fRec.getRecommendation(user5);
-        ArrayList<User> friends = new ArrayList<>();
-        friends = user5.getFriends();
-        for (int i = 0; i < recs.size(); i++) {
-            //System.out.println(recs.get(i));
-            friends.add(recs.get(i));
-        }
-        //System.out.println(friends.size());
-        curUser.setFriends(friends);
-        assertEquals(5, curUser.getFriends().size()); //adds repeat friends????
-    }
-
-    @Test
-    void getRecommend() {
-        ArrayList<User> keys = new ArrayList<>(accountManager.getGraph().accounts.keySet());
-        System.out.println(keys);
-        FriendRecommender fRec = new FriendRecommender();
-        User rec = fRec.getRecommend(keys.get(0), graph);
-        assertEquals(keys.get(4), rec); //Get the most common friend between your friends
-
-    }
 
     @Test
     void RenewTest(){
