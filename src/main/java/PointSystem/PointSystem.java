48a7ff62364ee5e01afaeda1b4041fa2950a59f7
ak <ariankhademi123@gmail.com>
2022-11-20 16:22:00 -0500

    Fixed previous commit errors

---
+++ src/main/java/PointSystem/PointSystem.java
@@ -0,0 +1,11 @@
+package PointSystem;
+
+import entities.User;
+// aaa
+public abstract class PointSystem {
+    /**
+     * Abstract parent class for the two different PointSystem classes .
+     */
+
+    public abstract void adjustPoints(User X, String spendCase);
+}
