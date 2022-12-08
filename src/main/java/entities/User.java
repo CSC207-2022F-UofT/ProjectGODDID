229b97e313341801b5edcbae4701657c9b3f3a15
Asr161 <ashvat.ranadive@mail.utoronto.ca>
2022-12-08 13:34:27 -0500

    added docstring to User

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 import static useCases.AccountManager.*;
 
+/**
+ * User class which is the object (entity) where the users of the app is stored as
+ */
 public class User implements Serializable {
     public ArrayList<User> friends;
     private String username, password;
@@ -15,9 +18,7 @@ public class User implements Serializable {
     public int num_strikes;
 
     /**
-     * @param name
-     *
-     * User class which is the object (entity) where the users of the app is stored as
+     * @param name username
      */
     public User(String name){
         this.username = name;
@@ -25,50 +26,86 @@ public class User implements Serializable {
         this.friends = new ArrayList<User>();
     }
 
+    /**
+     * setter method to set the username of the user as the attribute
+     * @param name username
+     */
     public void setUsername(String name){
         username = name;
     }
 
 
+    /**
+     * setter method to set the password of the user as the attribute
+     * @param pWord password of the user
+     */
     public void setPassword(String pWord){
         password = pWord;
     }
 
+    /**
+     * setter method to set the account type of the user as the attribute
+     * @param acc_type account type of the user
+     */
     public void setAccountType(String acc_type){
         this.acc_type = acc_type;
     }
 
+    /**
+     * setter method to set the friends of the user as the attribute
+     * @param friends list of friends of the user
+     */
     public void setFriends(ArrayList<User> friends) {
         this.friends = friends;
     }
 
-    public void setPoints(int p) {
 
-        this.points = p;
-    }
-
+    /**
+     * setter method to set the number of strikes of the user as the attribute
+     * @param num_strike number of strikes associated with the user
+     */
     public void setNum_strikes(int num_strike){
         this.num_strikes = num_strike;
     }
 
+    /**
+     * getter method to return the username of the user
+     * @return username of the user
+     */
     public String getUsername() {
         return username;
     }
 
 
+    /**
+     * getter method to return the points of the user
+     * @return number of points the user has
+     */
     public int getPoints() {
         return this.points;
     }
 
 
+    /**
+     * getter method to return the password of the user
+     * @return password of the user
+     */
     public String getPassword(  ){
         return password;
     }
 
+    /**
+     * getter method to return the friends of the user
+     * @return list of friends of the user
+     */
     public ArrayList<User> getFriends() {
         return friends;
     }
 
+    /**
+     * getter method to return the number of strikes of the user
+     * @return number of strikes
+     */
     public int getNum_strikes(){
         return num_strikes;
     }
